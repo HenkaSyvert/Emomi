eef4ef9c2a18a7a2406d69eef3d45e1c98500160
henrik syvertsson <henrik.syvertsson@gmail.com>
2026-03-27 20:01:41 -0600

    reorganise sprites/ folders

---
+++ scenes/primitives/question_drag_target/question_drag_target.gd
@@ -0,0 +1,27 @@
+@tool
+class_name QuestionDragTarget
+extends Control
+
+signal answered(is_correct: bool)
+
+@export var frames: Array[Texture2D]
+@export var wrong_frame: Texture2D
+
+@onready var _anim: AnimatedTextureRect
+
+
+func _ready() -> void:
+	_anim = find_child("AnimatedTextureRect")
+	_anim.frames = frames
+
+
+func _on_drag_target_drag_data_dropped(data: Variant) -> void:
+	if data is bool:
+		var is_correct: bool = data
+
+		if not is_correct and wrong_frame:
+			_anim.stop()
+			_anim.play()
+			_anim.texture = wrong_frame
+
+		answered.emit(is_correct)
